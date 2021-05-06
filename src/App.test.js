86ce1db3e3d1705cb081b721ef2ca65b29082efb
Nic <42087659+nicm42@users.noreply.github.com>
2021-05-06 16:44:44 +0100

    Mocked dummy card content

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const cards = [
+  {
+    id: 'id 1',
+    cover:
+      'http://books.google.com/books/content?id=f280CwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    title: 'Harry Potter: The Complete Collection (1-7)',
+    author: 'J.K. Rowling',
+    publisher: 'Pottermore Publishing',
+    link:
+      'https://play.google.com/store/books/details?id=f280CwAAQBAJ&source=gbs_api',
+  },
+  {
+    id: 'id 2',
+    cover:
+      'http://books.google.com/books/content?id=N6DeDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    title: 'Harry Potter and the Deathly Hallows',
+    author: 'J. K. Rowling',
+    publisher: 'Bloomsbury Publishing',
+    link:
+      'http://books.google.co.uk/books?id=N6DeDQAAQBAJ&dq=harry+potter&hl=&source=gbs_api',
+  },
+];
+
+jest.mock('./components/dummyCards', () => ({
+  cards,
+}));
+
 describe('App tests', () => {
   it('renders without crashing', () => {
     render(<App />);
--- src/components/Books.test.js
+++ src/components/Books.test.js
@@ -1,6 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import Books from './Books';
 
+const cards = [
+  {
+    id: 'id 1',
+    cover:
+      'http://books.google.com/books/content?id=f280CwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    title: 'Harry Potter: The Complete Collection (1-7)',
+    author: 'J.K. Rowling',
+    publisher: 'Pottermore Publishing',
+    link:
+      'https://play.google.com/store/books/details?id=f280CwAAQBAJ&source=gbs_api',
+  },
+  {
+    id: 'id 2',
+    cover:
+      'http://books.google.com/books/content?id=N6DeDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    title: 'Harry Potter and the Deathly Hallows',
+    author: 'J. K. Rowling',
+    publisher: 'Bloomsbury Publishing',
+    link:
+      'http://books.google.co.uk/books?id=N6DeDQAAQBAJ&dq=harry+potter&hl=&source=gbs_api',
+  },
+];
+
+jest.mock('./dummyCards', () => ({
+  cards,
+}));
+
 describe('Search tests', () => {
   it('renders without crashing', () => {
     render(<Books />);
@@ -9,7 +36,7 @@ describe('Search tests', () => {
   it('has has some cards', () => {
     render(<Books />);
     const card = screen.getAllByTestId('card');
-    expect(card).toHaveLength(3);
+    expect(card).toHaveLength(2);
   });
 
   it('has all the expected information in the cards', () => {
@@ -19,10 +46,10 @@ describe('Search tests', () => {
     const author = screen.getAllByTestId('author');
     const publisher = screen.getAllByTestId('publisher');
     const moreInfo = screen.getAllByRole('link');
-    expect(coverImage).toHaveLength(3);
-    expect(title).toHaveLength(3);
-    expect(author).toHaveLength(3);
-    expect(publisher).toHaveLength(3);
-    expect(moreInfo).toHaveLength(3);
+    expect(coverImage).toHaveLength(2);
+    expect(title).toHaveLength(2);
+    expect(author).toHaveLength(2);
+    expect(publisher).toHaveLength(2);
+    expect(moreInfo).toHaveLength(2);
   });
 });
