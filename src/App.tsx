b3cc8df2e3fb8893adf0965977db109ecbb79c23
Nic <42087659+nicm42@users.noreply.github.com>
2021-05-08 15:22:26 +0100

    Updated state with API data

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,21 @@ import Card from './components/Card';
 import { Books } from './App.style';
 //import cardData from './components/dummyCards';
 
+/* interface cards {
+  id: string;
+  cover: string;
+  title: string;
+  author: string;
+  publisher: string;
+  link: string;
+} */
+
 const App = () => {
-  const [cardData, setCardData] = useState([
+  /* const [cardData, setCardData] = useState([
     { id: 'id', cover: '', title: '', author: '', publisher: '', link: '' },
-  ]);
+  ]);*/
+  const [cardData, setCardData] = useState<any[]>([]);
+  //const [cardData, setCardData] = useState<null | { id: string }>([]);
 
   useEffect(() => {
     document.title = 'Book Finder';
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -9,6 +9,7 @@ interface SearchProps {
 const Search = ({ setCardData }: SearchProps) => {
   const [searchText, setSearchText] = useState<string>('');
 
+  //TODO should this be in useEffect?
   const getData = async (event: React.FormEvent<EventTarget>) => {
     event.preventDefault();
     setSearchText(''); //clear input now we're submitting
@@ -18,24 +19,46 @@ const Search = ({ setCardData }: SearchProps) => {
       const response = await axios.get(
         `https://www.googleapis.com/books/v1/volumes?q=${searchText}`
       );
+      //console.log(response.data.items[1]);
       /* console.log(response.data.items.length);
       console.log(response.data.items[1].id);
       console.log(response.data.items[1].volumeInfo.imageLinks.thumbnail);
       console.log(response.data.items[1].volumeInfo.title);
       console.log(response.data.items[1].volumeInfo.authors[0]);
-      console.log(response.data.items[1].volumeInfo.publisher);
+      console.log(response.data.items[1].volumeInfo.publisher); 
       console.log(response.data.items[1].volumeInfo.infoLink); */
-      console.log(response.data.items);
-      setCardData([
+      /* response.data.items.forEach((book: any) => {
+        setCardData({
+          id: book.id,
+          //cover: book.volumeInfo.imageLinks.thumbnail,
+          title: book.volumeInfo.title,
+          author: book.volumeInfo.authors[0],
+          publisher: book.volumeInfo.publisher,
+          link: book.volumeInfo.infoLink,
+        });
+      }); */
+      /* response.data.items.forEach((book: any) => {
+        console.log({
+          id: book.id,
+          cover: book.volumeInfo.imageLinks.thumbnail,
+          title: book.volumeInfo.title,
+          author: book.volumeInfo.authors[0],
+          publisher: book.volumeInfo.publisher,
+          link: book.volumeInfo.infoLink,
+        });
+      }); */
+      //TODO update the any on book
+      /* setCardData(response.data.items.forEach((book: any) => {
         {
-          id: response.data.items[1].id,
-          cover: response.data.items[1].volumeInfo.imageLinks.thumbnail,
-          title: response.data.items[1].volumeInfo.title,
-          author: response.data.items[1].volumeInfo.authors[0],
-          publisher: response.data.items[1].volumeInfo.publisher,
-          link: response.data.items[1].volumeInfo.infoLink,
-        },
-      ]);
+          id: book.id,
+          cover: book.volumeInfo.imageLinks.thumbnail,
+          title: book.volumeInfo.title,
+          author: book.volumeInfo.authors[0],
+          publisher: book.volumeInfo.publisher,
+          link: book.volumeInfo.infoLink,
+        }
+      })); */
+      setCardData(response.data.items);
     } catch (error) {
       console.error(error);
     }
