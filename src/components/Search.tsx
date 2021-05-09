7c5f1c261ae2efae7ac927064feaa5131c4f5c2e
Nic <42087659+nicm42@users.noreply.github.com>
2021-05-09 14:34:51 +0100

    Cleared out commented out crap

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -13,13 +13,10 @@ const Search = ({ setCardData }: SearchProps) => {
   const getData = async (event: React.FormEvent<EventTarget>) => {
     event.preventDefault();
     setSearchText(''); //clear input now we're submitting
-    //console.log(searchText);
-    //console.log(`https://www.googleapis.com/books/v1/volumes?q=${searchText}`);
     try {
       const response = await axios.get(
         `https://www.googleapis.com/books/v1/volumes?q=${searchText}`
       );
-      //console.log(response.data.items[1]);
       /* console.log(response.data.items.length);
       console.log(response.data.items[1].id);
       console.log(response.data.items[1].volumeInfo.imageLinks.thumbnail);
@@ -27,37 +24,6 @@ const Search = ({ setCardData }: SearchProps) => {
       console.log(response.data.items[1].volumeInfo.authors[0]);
       console.log(response.data.items[1].volumeInfo.publisher); 
       console.log(response.data.items[1].volumeInfo.infoLink); */
-      /* response.data.items.forEach((book: any) => {
-        setCardData({
-          id: book.id,
-          //cover: book.volumeInfo.imageLinks.thumbnail,
-          title: book.volumeInfo.title,
-          author: book.volumeInfo.authors[0],
-          publisher: book.volumeInfo.publisher,
-          link: book.volumeInfo.infoLink,
-        });
-      }); */
-      /* response.data.items.forEach((book: any) => {
-        console.log({
-          id: book.id,
-          cover: book.volumeInfo.imageLinks.thumbnail,
-          title: book.volumeInfo.title,
-          author: book.volumeInfo.authors[0],
-          publisher: book.volumeInfo.publisher,
-          link: book.volumeInfo.infoLink,
-        });
-      }); */
-      //TODO update the any on book
-      /* setCardData(response.data.items.forEach((book: any) => {
-        {
-          id: book.id,
-          cover: book.volumeInfo.imageLinks.thumbnail,
-          title: book.volumeInfo.title,
-          author: book.volumeInfo.authors[0],
-          publisher: book.volumeInfo.publisher,
-          link: book.volumeInfo.infoLink,
-        }
-      })); */
       setCardData(response.data.items);
     } catch (error) {
       console.error(error);
