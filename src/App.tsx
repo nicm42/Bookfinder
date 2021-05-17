d4a38877536d83f4f89dc3b3fcdf89b8b86dd343
Nic <42087659+nicm42@users.noreply.github.com>
2021-05-17 17:00:59 +0100

    Moved getData from Search to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Search from './components/Search';
 import Card from './components/Card';
 import { Loading, Error, Books, CardDiv } from './App.style';
@@ -24,13 +25,35 @@ const App = () => {
     }
   }, [cardData]); */
 
+  const getData = async (search: string) => {
+    setIsLoading(true);
+    setCardData([]); //in case this is another search, clear the results from the previous search
+    try {
+      //TODO will need to repeat this if there are more than 10 results
+      //Although how many do we want to show on the page? Maybe a link to get more books?
+      //Or just get 10 at a time
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${search}`
+      );
+      //console.log(response.data.totalItems);
+      /* console.log(response.data.items[1].id);
+      console.log(response.data.items[1].volumeInfo.imageLinks.thumbnail);
+      console.log(response.data.items[1].volumeInfo.title);
+      console.log(response.data.items[1].volumeInfo.authors[0]);
+      console.log(response.data.items[1].volumeInfo.publisher); 
+      console.log(response.data.items[1].volumeInfo.infoLink); */
+      setCardData(response.data.items);
+      //setIsError(`No books were found for ${search} :(`); //uncomment to test nothing returned from API
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsError(error); //TODO why does this line make Jest App test unhappy?
+    }
+  };
+
   return (
     <>
-      <Search
-        setCardData={setCardData}
-        setIsLoading={setIsLoading}
-        setIsError={setIsError}
-      />
+      <Search getData={getData} />
       {isLoading && <Loading data-testid="loading">Loading</Loading>}{' '}
       {/* TODO replace this text with an animation */}
       {isError && <Error data-testid="error">{isError}</Error>}
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,49 +1,27 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
+//import axios from 'axios';
 import { Label, Input, Submit } from './Search.styles';
 
 interface SearchProps {
-  setCardData: Function;
+  /* setCardData: Function;
   setIsLoading: Function;
-  setIsError: Function;
+  setIsError: Function; */
+  getData: Function;
 }
 
-const Search = ({ setCardData, setIsLoading, setIsError }: SearchProps) => {
+const Search = ({ getData }: SearchProps) => {
   const [searchText, setSearchText] = useState<string>('');
 
-  //TODO should this be in useEffect?
-  const getData = async (event: React.FormEvent<EventTarget>) => {
+  const submitSearch = async (event: React.FormEvent<EventTarget>) => {
     event.preventDefault();
-    setIsLoading(true);
-    setCardData([]); //in case this is another search, clear the results from the previous search
     const search = searchText;
     setSearchText(''); //clear input now we're submitting
-    try {
-      //TODO will need to repeat this if there are more than 10 results
-      //Although how many do we want to show on the page? Maybe a link to get more books?
-      //Or just get 10 at a time
-      const response = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=${search}`
-      );
-      //console.log(response.data.totalItems);
-      /* console.log(response.data.items[1].id);
-      console.log(response.data.items[1].volumeInfo.imageLinks.thumbnail);
-      console.log(response.data.items[1].volumeInfo.title);
-      console.log(response.data.items[1].volumeInfo.authors[0]);
-      console.log(response.data.items[1].volumeInfo.publisher); 
-      console.log(response.data.items[1].volumeInfo.infoLink); */
-      setCardData(response.data.items);
-      //setIsError(`No books were found for ${search} :(`); //uncomment to test nothing returned from API
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-      setIsError(error); //TODO why does this line make Jest App test unhappy?
-    }
+    getData(search);
   };
 
   return (
-    <form onSubmit={(e) => getData(e)}>
+    <form onSubmit={(e) => submitSearch(e)}>
       <Label htmlFor="search-input">Search for a book </Label>
       <Input
         type="search"
@@ -60,8 +38,12 @@ const Search = ({ setCardData, setIsLoading, setIsError }: SearchProps) => {
   );
 };
 
-Search.propTypes = {
+/* Search.propTypes = {
   setCardData: PropTypes.func.isRequired,
+}; */
+
+Search.propTypes = {
+  getData: PropTypes.func.isRequired,
 };
 
 export default Search;
