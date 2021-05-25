f9ba7faef28cc4980ebcad9401722b823445227a
Nic <42087659+nicm42@users.noreply.github.com>
2021-05-25 17:22:11 +0100

    Added font

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -2,9 +2,14 @@ import { createGlobalStyle } from 'styled-components/macro';
 import { colours } from './constants';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
+
   body {
+    margin: 0;
+    padding: 1em;
     background-color: ${colours.background};
-    color: ${colours.text}
+    color: ${colours.text};
+    font-family: 'Poppins', sans-serif;
   }
 `;
 
