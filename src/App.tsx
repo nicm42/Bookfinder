ea32aacf6455cc70cbb9f86830d46db6c9c3fa35
Nic <42087659+nicm42@users.noreply.github.com>
2021-05-31 17:48:18 +0100

    Clear out the data array when doing a new search

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import axios from 'axios';
 import Search from './components/Search';
 import Card from './components/Card';
 import * as Styled from './App.style';
-import { testCards } from './dummyCardData'; //uncomment to load cards without using API
+//import { testCards } from './dummyCardData'; //uncomment to load cards without using API
 
 const App = () => {
-  //const [cardData, setCardData] = useState<any[]>([]);
-  const [cardData, setCardData] = useState<any[]>(testCards); //uncomment to load cards without using API
+  const [cardData, setCardData] = useState<any[]>([]);
+  //const [cardData, setCardData] = useState<any[]>(testCards); //uncomment to load cards without using API
   const [results, setResults] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<Boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>('');
@@ -15,8 +15,8 @@ const App = () => {
   const [searchText, setSearchText] = useState<string>('');
   const [searchType, setSearchType] = useState<string>('');
 
-  const [isPreviousResults, setIsPreviousResults] = useState<boolean>(true);
-  const [isMoreResults, setIsMoreResults] = useState<boolean>(true);
+  const [isPreviousResults, setIsPreviousResults] = useState<boolean>(false);
+  const [isMoreResults, setIsMoreResults] = useState<boolean>(false);
   const [pageNumber, setPageNumber] = useState<number>(0);
 
   const [resultStart, setResultStart] = useState<number>(-9);
@@ -99,7 +99,7 @@ const App = () => {
       setIsMoreResults(false);
       setIsPreviousResults(false);
       setTotalItems(0);
-      //setResultCount(0);
+      setResults([]);
     } else {
       setIsPreviousResults(true);
     }
