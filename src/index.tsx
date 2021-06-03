a9ba857f25061c33592713315f5b37327ba21384
Nic <42087659+nicm42@users.noreply.github.com>
2021-06-03 09:56:48 +0100

    Uninstalled Helmet and moved header stuff to index.html

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import GlobalStyle from './globalStyles';
-import favicon from './images/favicon.ico';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <HelmetProvider>
-      <Helmet>
-        <title>Book Search</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" type="image/ico" href={favicon} />
-      </Helmet>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
