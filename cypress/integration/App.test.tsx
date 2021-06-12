3d5be2709ef9725f0698babdbab048ae0ce7dc03
Nic <42087659+nicm42@users.noreply.github.com>
2021-06-12 17:57:03 +0100

    Fixed dummy test so it passes

--- cypress/integration/App.test.tsx
+++ cypress/integration/App.test.tsx
@@ -1,3 +1,13 @@
-it('should work', () => {
-  cy.get('h1').should('have.text', 'Book Search');
+/// <reference types="Cypress" />
+
+describe('TODO add description', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line no-undef
+    cy.visit(Cypress.config('baseUrl'));
+  });
+
+  it('should work', () => {
+    // eslint-disable-next-line no-undef
+    cy.get('h1').should('have.text', 'Book Search');
+  });
 });
