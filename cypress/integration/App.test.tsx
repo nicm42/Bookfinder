dbd3dd501af1343cdeb0b467b84874ccb097182f
Nic <42087659+nicm42@users.noreply.github.com>
2021-06-14 14:10:23 +0100

    Fixed Jest and Cypress conflicts

--- cypress/integration/App.test.tsx
+++ cypress/integration/App.test.tsx
@@ -2,12 +2,10 @@
 
 describe('TODO add description', () => {
   beforeEach(() => {
-    // eslint-disable-next-line no-undef
     cy.visit(Cypress.config('baseUrl'));
   });
 
   it('should work', () => {
-    // eslint-disable-next-line no-undef
     cy.get('h1').should('have.text', 'Book Search');
   });
 });
