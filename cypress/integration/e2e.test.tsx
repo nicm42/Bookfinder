f752e081a5273d6cec0bed2b82baa9f24df29371
Nic <42087659+nicm42@users.noreply.github.com>
2021-06-14 20:48:41 +0100

    Added test for getting cards

--- cypress/integration/e2e.test.tsx
+++ cypress/integration/e2e.test.tsx
@@ -1,6 +1,8 @@
 /// <reference types="Cypress" />
 /// <reference types="@testing-library/cypress" />
 
+const api = 'https://www.googleapis.com/books/v1/volumes?q=';
+
 describe('E2E tests', () => {
   beforeEach(() => {
     cy.visit(Cypress.config('baseUrl'));
@@ -19,7 +21,7 @@ describe('E2E tests', () => {
     cy.findByRole('button', { name: /search/i }).should('exist');
   });
 
-  it.only('should not let you submit if option and/or input are blank', () => {
+  it('should not let you submit if option and/or input are blank', () => {
     cy.findByRole('button', { name: /search/i }).click();
     cy.get('select:invalid').then(($combobox) => {
       expect($combobox[0].validationMessage).to.eq(
@@ -30,5 +32,22 @@ describe('E2E tests', () => {
     cy.get('input:invalid').then(($input) => {
       expect($input[0].validationMessage).to.eq('Please fill in this field.');
     });
+  });
+
+  it.only('gets one page of cards', () => {
+    cy.intercept('GET', `${api}intitle:%22test%22&startIndex=0&maxResults=10`, {
+      fixture: 'cards.json',
+    }).as('getData');
+    cy.findByTestId('select').select('intitle');
+    cy.findByRole('searchbox').type('test');
+    cy.findByRole('button', { name: /search/i }).click();
+    //cy.findByTestId('loading').should('exist'); //TODO how to convince it this appears before cards are loaded
+    cy.wait('@getData');
+    cy.findByText('Showing books 1-4').should('exist');
+    cy.findByTestId('loading').should('not.exist');
+    cy.findByTestId('select').should('have.value', '');
+    cy.findByRole('searchbox').should('have.value', '');
+    cy.findByRole('button', { name: /previous/i }).should('not.exist');
+    cy.findByRole('button', { name: /next/i }).should('not.exist');
   });
 });
