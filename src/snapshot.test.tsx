16aa22cd14f9b19a485e371e12d5c7b6469c3432
Nic <42087659+nicm42@users.noreply.github.com>
2021-06-25 17:11:31 +0100

    Fixed snapshots

--- src/snapshot.test.tsx
+++ src/snapshot.test.tsx
@@ -6,6 +6,8 @@ import Loading from './components/Loading';
 import Error from './components/Error';
 import Results from './components/Results';
 import NavButtons from './components/NavButtons';
+import ButtonContext from './contexts/ButtonContext';
+import CountContext from './contexts/CountContext';
 import cards from './dummyCardData';
 
 describe('Snapshot tests', () => {
@@ -36,63 +38,138 @@ describe('Snapshot tests', () => {
   });
 
   it('renders results text correctly', () => {
+    const resultStart = 1;
+    const setResultStart = jest.fn();
+    const resultEnd = 10;
+    const setResultEnd = jest.fn();
+
     const tree = renderer
-      .create(<Results resultStart={1} resultEnd={10} />)
+      .create(
+        <CountContext.Provider
+          value={{ resultStart, setResultStart, resultEnd, setResultEnd }}
+        >
+          <Results />
+        </CountContext.Provider>
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders nav buttons correctly for both buttons', () => {
+    const isPreviousResults = true;
+    const setIsPreviousResults = jest.fn();
+    const isMoreResults = true;
+    const setIsMoreResults = jest.fn();
+    const setCardData = jest.fn();
+
     const tree = renderer
       .create(
-        <NavButtons
-          isPreviousResults
-          isMoreResults
-          searchAgain={searchAgain}
-          goBack={goBack}
-        />
+        <ButtonContext.Provider
+          value={{
+            isPreviousResults,
+            setIsPreviousResults,
+            isMoreResults,
+            setIsMoreResults,
+          }}
+        >
+          <NavButtons
+            getData={getData}
+            resultsPerPage={10}
+            results={[]}
+            setCardData={setCardData}
+            totalItems={10}
+          />
+        </ButtonContext.Provider>
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders nav buttons correctly for just next button', () => {
+    const isPreviousResults = false;
+    const setIsPreviousResults = jest.fn();
+    const isMoreResults = true;
+    const setIsMoreResults = jest.fn();
+    const setCardData = jest.fn();
+
     const tree = renderer
       .create(
-        <NavButtons
-          isPreviousResults={false}
-          isMoreResults
-          searchAgain={searchAgain}
-          goBack={goBack}
-        />
+        <ButtonContext.Provider
+          value={{
+            isPreviousResults,
+            setIsPreviousResults,
+            isMoreResults,
+            setIsMoreResults,
+          }}
+        >
+          <NavButtons
+            getData={getData}
+            resultsPerPage={10}
+            results={[]}
+            setCardData={setCardData}
+            totalItems={10}
+          />
+        </ButtonContext.Provider>
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders nav buttons correctly for just previous button', () => {
+    const isPreviousResults = true;
+    const setIsPreviousResults = jest.fn();
+    const isMoreResults = false;
+    const setIsMoreResults = jest.fn();
+    const setCardData = jest.fn();
+
     const tree = renderer
       .create(
-        <NavButtons
-          isPreviousResults
-          isMoreResults={false}
-          searchAgain={searchAgain}
-          goBack={goBack}
-        />
+        <ButtonContext.Provider
+          value={{
+            isPreviousResults,
+            setIsPreviousResults,
+            isMoreResults,
+            setIsMoreResults,
+          }}
+        >
+          <NavButtons
+            getData={getData}
+            resultsPerPage={10}
+            results={[]}
+            setCardData={setCardData}
+            totalItems={10}
+          />
+        </ButtonContext.Provider>
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('renders nav buttons correctly for no buttons', () => {
+    const isPreviousResults = false;
+    const setIsPreviousResults = jest.fn();
+    const isMoreResults = false;
+    const setIsMoreResults = jest.fn();
+    const setCardData = jest.fn();
+
     const tree = renderer
       .create(
-        <NavButtons
-          isPreviousResults={false}
-          isMoreResults={false}
-          searchAgain={searchAgain}
-          goBack={goBack}
-        />
+        <ButtonContext.Provider
+          value={{
+            isPreviousResults,
+            setIsPreviousResults,
+            isMoreResults,
+            setIsMoreResults,
+          }}
+        >
+          <NavButtons
+            getData={getData}
+            resultsPerPage={10}
+            results={[]}
+            setCardData={setCardData}
+            totalItems={10}
+          />
+        </ButtonContext.Provider>
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
